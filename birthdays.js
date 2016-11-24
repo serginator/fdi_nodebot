56fa1d924da4ff5dc286b7b40bf7dfe9da633984
Sergio Ruiz <serginator@gmail.com>
2016-11-24 10:18:33 +0100

    Initial commit

---
+++ birthdays.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'Ana': '5/8',
+    'Antuan': '17/4',
+    'Bea': '4/8',
+    'Cadenas': '18/6',
+    'Dani': '18/12',
+    'Diego': '18/6',
+    'Kike': '19/12',
+    'Felix': '20/11',
+    'Iñigo': '22/11',
+    'Jachu': '26/1',
+    'Jorge': '14/3',
+    'Juanlu': '11/11',
+    'Manu': '20/6',
+    'Miguel': '25/7',
+    'Sergio': '9/12',
+    'Victor': '16/3'
+};
