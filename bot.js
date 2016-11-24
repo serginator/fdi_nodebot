ff830c770087c8a3e137ef7d5db9acee929879df
Sergio Ruiz <serginator@gmail.com>
2016-11-24 15:18:55 +0100

    Added /siguiente command

--- bot.js
+++ bot.js
@@ -30,6 +30,38 @@ function getListOfBirthdays() {
     return msg;
 }
 
+function getNext() {
+    var date = new Date(),
+        dateTime = date.getTime(),
+        nextDate = dateTime,
+        msg = '',
+        res;
+    for (var i in birthdays) {
+        if (birthdays.hasOwnProperty(i)) {
+            var aux = birthdays[i].split('/'),
+                dateAux = (new Date(date.getFullYear(), aux[1] - 1, aux[0])),
+                dateAuxTime = dateAux.getTime();
+            if ((dateAuxTime - dateTime) > 0 && (dateAuxTime - dateTime) < nextDate) {
+                nextDate = dateAuxTime - dateTime;
+                res = [i];
+            } else if ((dateAuxTime - dateTime) === nextDate) {
+                nextDate = dateAuxTime - dateTime;
+                res.push(i);
+            }
+        }
+    }
+    if (res.length === 1) {
+        msg += 'El próximo cumpleaños es el día ' + birthdays[res[0]] + ', de ' + res[0];
+    } else {
+        msg += 'El próximo cumpleaños es el día ' + birthdays[res[0]] + ', de ' + res[0];
+        for (var i = 1, n = res.length; i < n; i++) {
+            msg += ' y ' + res[i];
+        }
+    }
+
+    return msg;
+}
+
 function help() {
     var msg = '';
 
@@ -37,6 +69,7 @@ function help() {
     msg += 'También me podéis dar alguna orden:\n\n';
     msg += '/ayuda: Este comando\n';
     msg += '/lista: Lista de cumpleaños\n';
+    msg += '/siguiente: Próximo cumpleaños\n';
     return msg;
 }
 
@@ -65,9 +98,16 @@ bot.onText(/\/lista/, function (msg, match) {
   bot.sendMessage(chatId, getListOfBirthdays());
 });
 
+// Matches "/ayuda"
 bot.onText(/\/ayuda/, function (msg, match) {
     var chatId = msg.chat.id;
     bot.sendMessage(chatId, help());
+});
+
+// Matches "/siguiente"
+bot.onText(/\/siguiente/, function (msg, match) {
+    var chatId = msg.chat.id;
+    bot.sendMessage(chatId, getNext());
 });
 
 console.log('Bot server started...');
